e1f24ed250cc3bcd516135d396ac5cff0c07cb84
William Low <wlwleong@gmail.com>
2022-04-21 18:26:18 +0800

    fixing memory leaks

--- test_mains/init_linked_list.c
+++ test_mains/init_linked_list.c
@@ -13,46 +13,57 @@
 #include "../libft/libft.h"
 #include <stdio.h>
 
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_back(t_list **lst, t_list *new);
+t_list		*ft_lstnew(void *content);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+int			ft_lstsize(t_list *lst);
+void		ft_lstclear_new(t_list **lst);
 
 int	main(int argc, char *argv[])
 {
 	t_list	*stack_a;
 	t_list	*stack_a_head;
-	int		*stack_a_array;
 	int		i;
 
 	if (argc < 2)
 		exit(EXIT_FAILURE);
-	stack_a_array = malloc (sizeof(int) * argc - 1);
-	if (!stack_a_array)
-		exit(EXIT_FAILURE);
-	i = 0;
-	while (i < argc - 1)
-	{
-		stack_a_array[i] = ft_atoi(argv[i + 1]);
-		i++;
-	}
 	i = 1;
 	while (i < argc)
 	{
 		if (i == 1)
 		{
-			stack_a = ft_lstnew(&stack_a_array[i - 1]);
+			stack_a = ft_lstnew(argv[i]);
 			stack_a_head = stack_a;
 		}
 		else
-			ft_lstadd_back(&stack_a, ft_lstnew(&stack_a_array[i - 1]));
+			ft_lstadd_back(&stack_a, ft_lstnew(argv[i]));
 		i++;
 	}
-	stack_a = stack_a_head;
+	printf("stack_a size is %d\n", ft_lstsize(stack_a));
 	i = 1;
 	while (stack_a)
 	{
-		printf("Node %d = %d\n", i, *(int *)stack_a->content);
+		printf("Node %d = %s\n", i, (char *)stack_a->content);
 		stack_a = stack_a->next;
 		i++;
 	}
+	stack_a = stack_a_head;
+	ft_lstclear_new(&stack_a);
 	return (0);
+}
+
+void	ft_lstclear_new(t_list **lst)
+{
+	t_list	*list;
+	t_list	*temp;
+
+	list = *lst;
+	if (!list)
+		return ;
+	while (list)
+	{
+		temp = list->next;
+		free(list);
+		list = temp;
+	}
+	*lst = NULL;
 }
